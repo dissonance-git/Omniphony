ded9c7eb419165cb0ed63b8fb5f217957410e7f3
dissonance <ustreamusa@aim.com>
2026-08-12 11:55:08 -0400

    feat(audio): register HRTF early-reflection field

--- omniphony-renderer/windows_host/src/bin/omniphony_app.rs
+++ omniphony-renderer/windows_host/src/bin/omniphony_app.rs
@@ -5,6 +5,10 @@
 mod supervisor;
 
 #[cfg(target_os = "windows")]
+#[path = "../music_early_reflections.rs"]
+mod music_early_reflections;
+
+#[cfg(target_os = "windows")]
 #[path = "../music_support.rs"]
 mod music_support;
 
