ccb70b0244c542658ac27d591923606fc649012f
dissonance <ustreamusa@aim.com>
2026-08-19 13:03:47 -0400

    feat(windows): snapshot spatial endpoint provider state

--- omniphony-renderer/windows_installer/endpoint_apo/OmniphonySpatialProviderProbe.cpp
+++ omniphony-renderer/windows_installer/endpoint_apo/OmniphonySpatialProviderProbe.cpp
@@ -1,9 +1,11 @@
 #include <windows.h>
 #include <spatialaudioclient.h>
 
+#include <algorithm>
 #include <cwchar>
 #include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -13,6 +15,8 @@ constexpr wchar_t kEncoderPath[] =
     L"SOFTWARE\\Microsoft\\Multimedia\\Audio\\Spatial\\Encoder";
 constexpr wchar_t kSpatialEndpointPath[] =
     L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\MMDevices\\SpatialAudioEndpoint";
+constexpr DWORD kSnapshotMaxDepth = 8;
+constexpr DWORD kSnapshotMaxValueBytes = 4096;
 
 std::wstring Win32Message(LONG code) {
     wchar_t buffer[512] = {};
@@ -87,6 +91,238 @@ LONG OpenReadOnly64(const wchar_t* path, HKEY* key) {
         0,
         KEY_READ | KEY_WOW64_64KEY,
         key);
+}
+
+std::wstring RegistryTypeName(DWORD type) {
+    switch (type) {
+        case REG_NONE:
+            return L"REG_NONE";
+        case REG_SZ:
+            return L"REG_SZ";
+        case REG_EXPAND_SZ:
+            return L"REG_EXPAND_SZ";
+        case REG_BINARY:
+            return L"REG_BINARY";
+        case REG_DWORD:
+            return L"REG_DWORD";
+        case REG_DWORD_BIG_ENDIAN:
+            return L"REG_DWORD_BIG_ENDIAN";
+        case REG_LINK:
+            return L"REG_LINK";
+        case REG_MULTI_SZ:
+            return L"REG_MULTI_SZ";
+        case REG_RESOURCE_LIST:
+            return L"REG_RESOURCE_LIST";
+        case REG_FULL_RESOURCE_DESCRIPTOR:
+            return L"REG_FULL_RESOURCE_DESCRIPTOR";
+        case REG_RESOURCE_REQUIREMENTS_LIST:
+            return L"REG_RESOURCE_REQUIREMENTS_LIST";
+        case REG_QWORD:
+            return L"REG_QWORD";
+        default:
+            return L"REG_UNKNOWN";
+    }
+}
+
+std::wstring BytesToHex(const std::vector<BYTE>& bytes) {
+    std::wostringstream out;
+    out << std::hex << std::uppercase << std::setfill(L'0');
+    for (BYTE value : bytes) {
+        out << std::setw(2) << static_cast<unsigned int>(value);
+    }
+    return out.str();
+}
+
+std::vector<std::wstring> SortedSubkeyNames(HKEY key, const std::wstring& prefix) {
+    DWORD subkeyCount = 0;
+    DWORD maxSubkeyLength = 0;
+    const LONG infoResult = RegQueryInfoKeyW(
+        key,
+        nullptr,
+        nullptr,
+        nullptr,
+        &subkeyCount,
+        &maxSubkeyLength,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr);
+    if (infoResult != ERROR_SUCCESS) {
+        std::wcout << prefix << L".subkey_query_error="
+                   << infoResult << L":" << Win32Message(infoResult) << L"\n";
+        return {};
+    }
+
+    std::vector<std::wstring> names;
+    names.reserve(subkeyCount);
+    std::vector<wchar_t> name(maxSubkeyLength + 2, L'\0');
+    for (DWORD index = 0; index < subkeyCount; ++index) {
+        DWORD nameLength = static_cast<DWORD>(name.size());
+        FILETIME lastWrite = {};
+        const LONG enumResult = RegEnumKeyExW(
+            key,
+            index,
+            name.data(),
+            &nameLength,
+            nullptr,
+            nullptr,
+            nullptr,
+            &lastWrite);
+        if (enumResult == ERROR_SUCCESS) {
+            names.emplace_back(name.data(), nameLength);
+        } else {
+            std::wcout << prefix << L".subkey_enum_error[" << index << L"]="
+                       << enumResult << L":" << Win32Message(enumResult) << L"\n";
+        }
+    }
+    std::sort(names.begin(), names.end());
+    return names;
+}
+
+std::vector<std::wstring> SortedValueNames(HKEY key, const std::wstring& prefix) {
+    DWORD valueCount = 0;
+    DWORD maxValueNameLength = 0;
+    const LONG infoResult = RegQueryInfoKeyW(
+        key,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        &valueCount,
+        &maxValueNameLength,
+        nullptr,
+        nullptr,
+        nullptr);
+    if (infoResult != ERROR_SUCCESS) {
+        std::wcout << prefix << L".value_query_error="
+                   << infoResult << L":" << Win32Message(infoResult) << L"\n";
+        return {};
+    }
+
+    std::vector<std::wstring> names;
+    names.reserve(valueCount);
+    std::vector<wchar_t> name(maxValueNameLength + 2, L'\0');
+    for (DWORD index = 0; index < valueCount; ++index) {
+        DWORD nameLength = static_cast<DWORD>(name.size());
+        const LONG enumResult = RegEnumValueW(
+            key,
+            index,
+            name.data(),
+            &nameLength,
+            nullptr,
+            nullptr,
+            nullptr,
+            nullptr);
+        if (enumResult == ERROR_SUCCESS) {
+            names.emplace_back(name.data(), nameLength);
+        } else {
+            std::wcout << prefix << L".value_enum_error[" << index << L"]="
+                       << enumResult << L":" << Win32Message(enumResult) << L"\n";
+        }
+    }
+    std::sort(names.begin(), names.end());
+    return names;
+}
+
+void PrintRegistryValues(HKEY key, const std::wstring& prefix) {
+    const auto names = SortedValueNames(key, prefix);
+    std::wcout << prefix << L".value_count=" << names.size() << L"\n";
+
+    for (size_t index = 0; index < names.size(); ++index) {
+        const std::wstring& name = names[index];
+        const wchar_t* queryName = name.empty() ? nullptr : name.c_str();
+        const std::wstring valuePrefix =
+            prefix + L".value[" + std::to_wstring(index) + L"]";
+        std::wcout << valuePrefix << L".name="
+                   << (name.empty() ? L"(Default)" : name) << L"\n";
+
+        DWORD type = REG_NONE;
+        DWORD byteCount = 0;
+        LONG queryResult = RegQueryValueExW(
+            key,
+            queryName,
+            nullptr,
+            &type,
+            nullptr,
+            &byteCount);
+        if (queryResult != ERROR_SUCCESS) {
+            std::wcout << valuePrefix << L".query_error="
+                       << queryResult << L":" << Win32Message(queryResult) << L"\n";
+            continue;
+        }
+
+        std::wcout << valuePrefix << L".type=" << RegistryTypeName(type)
+                   << L"(" << type << L")\n";
+        std::wcout << valuePrefix << L".byte_count=" << byteCount << L"\n";
+
+        if (byteCount > kSnapshotMaxValueBytes) {
+            std::wcout << valuePrefix << L".truncated=1\n";
+            std::wcout << valuePrefix << L".data_hex=<omitted>\n";
+            continue;
+        }
+
+        std::vector<BYTE> data(byteCount);
+        DWORD readBytes = byteCount;
+        if (byteCount > 0) {
+            queryResult = RegQueryValueExW(
+                key,
+                queryName,
+                nullptr,
+                &type,
+                data.data(),
+                &readBytes);
+            if (queryResult != ERROR_SUCCESS) {
+                std::wcout << valuePrefix << L".read_error="
+                           << queryResult << L":" << Win32Message(queryResult) << L"\n";
+                continue;
+            }
+            data.resize(readBytes);
+        }
+
+        std::wcout << valuePrefix << L".truncated=0\n";
+        std::wcout << valuePrefix << L".data_hex=" << BytesToHex(data) << L"\n";
+    }
+}
+
+void PrintRegistryTree(HKEY key, const std::wstring& prefix, DWORD depth) {
+    std::wcout << prefix << L".snapshot_depth=" << depth << L"\n";
+    PrintRegistryValues(key, prefix);
+
+    const auto names = SortedSubkeyNames(key, prefix);
+    std::wcout << prefix << L".snapshot_subkey_count=" << names.size() << L"\n";
+    if (depth >= kSnapshotMaxDepth) {
+        if (!names.empty()) {
+            std::wcout << prefix << L".snapshot_depth_limited=1\n";
+        }
+        return;
+    }
+
+    for (size_t index = 0; index < names.size(); ++index) {
+        const std::wstring& name = names[index];
+        const std::wstring childPrefix =
+            prefix + L".subkey[" + std::to_wstring(index) + L"]";
+        std::wcout << childPrefix << L".name=" << name << L"\n";
+
+        HKEY child = nullptr;
+        const LONG openResult = RegOpenKeyExW(
+            key,
+            name.c_str(),
+            0,
+            KEY_READ,
+            &child);
+        if (openResult != ERROR_SUCCESS) {
+            std::wcout << childPrefix << L".open_error="
+                       << openResult << L":" << Win32Message(openResult) << L"\n";
+            continue;
+        }
+
+        PrintRegistryTree(child, childPrefix, depth + 1);
+        RegCloseKey(child);
+    }
 }
 
 void ProbeProviderCom(const std::wstring& prefix, const std::wstring& clsidText) {
@@ -230,6 +466,10 @@ void ProbeEncoderRegistry(bool probeDirectCom) {
 
 void ProbeSpatialEndpointRegistry() {
     std::wcout << L"spatial_endpoint.path=HKLM\\" << kSpatialEndpointPath << L"\n";
+    std::wcout << L"spatial_endpoint.snapshot.read_only=1\n";
+    std::wcout << L"spatial_endpoint.snapshot.max_depth=" << kSnapshotMaxDepth << L"\n";
+    std::wcout << L"spatial_endpoint.snapshot.max_value_bytes=" << kSnapshotMaxValueBytes << L"\n";
+    std::wcout << L"spatial_endpoint.snapshot.encoding=registry_bytes_hex\n";
 
     HKEY root = nullptr;
     const LONG openResult = OpenReadOnly64(kSpatialEndpointPath, &root);
@@ -240,52 +480,7 @@ void ProbeSpatialEndpointRegistry() {
     }
 
     std::wcout << L"spatial_endpoint.status=available\n";
-
-    DWORD subkeyCount = 0;
-    DWORD maxSubkeyLength = 0;
-    const LONG infoResult = RegQueryInfoKeyW(
-        root,
-        nullptr,
-        nullptr,
-        nullptr,
-        &subkeyCount,
-        &maxSubkeyLength,
-        nullptr,
-        nullptr,
-        nullptr,
-        nullptr,
-        nullptr,
-        nullptr);
-    if (infoResult != ERROR_SUCCESS) {
-        std::wcout << L"spatial_endpoint.enumeration_error="
-                   << infoResult << L":" << Win32Message(infoResult) << L"\n";
-        RegCloseKey(root);
-        return;
-    }
-
-    std::wcout << L"spatial_endpoint.subkey_count=" << subkeyCount << L"\n";
-    std::vector<wchar_t> name(maxSubkeyLength + 2, L'\0');
-    for (DWORD index = 0; index < subkeyCount; ++index) {
-        DWORD nameLength = static_cast<DWORD>(name.size());
-        FILETIME lastWrite = {};
-        const LONG enumResult = RegEnumKeyExW(
-            root,
-            index,
-            name.data(),
-            &nameLength,
-            nullptr,
-            nullptr,
-            nullptr,
-            &lastWrite);
-        if (enumResult == ERROR_SUCCESS) {
-            std::wcout << L"spatial_endpoint.subkey[" << index << L"]="
-                       << std::wstring(name.data(), nameLength) << L"\n";
-        } else {
-            std::wcout << L"spatial_endpoint.subkey[" << index << L"].error="
-                       << enumResult << L":" << Win32Message(enumResult) << L"\n";
-        }
-    }
-
+    PrintRegistryTree(root, L"spatial_endpoint.snapshot", 0);
     RegCloseKey(root);
 }
 
